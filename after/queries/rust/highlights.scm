23c1a50d1b35514ededa60cb5e268423bf73da81
realbigsean <seananderson33@gmail.com>
2022-06-12 11:24:59 -0400

    test

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -1,18 +1,3 @@
-(source_file
-  (type_item
-    (type_identifier)
-    (generic_type
-      (type_identifier)
-      (type_arguments
-        (type_identifier) @label)))
-  (type_item
-    (type_identifier)
-    (generic_type
-      (type_identifier)
-      (type_arguments
-        (type_binding
-          (type_identifier) @label))))
   (type_item
     (constrained_type_parameter 
       left: (type_identifier) @label))
-)
