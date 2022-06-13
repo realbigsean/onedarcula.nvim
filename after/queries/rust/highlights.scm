e411583157337281392a6e88e8875c1036c9a4e7
realbigsean <seananderson33@gmail.com>
2022-06-12 20:33:56 -0400

    test

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -1,2 +1,5 @@
 (constrained_type_parameter 
   left: (type_identifier) @label)
+(scoped_identifier
+  path: (identifier) @path_name (#match? @path_name "^[A-Z][a-z_]+") 
+  name: (identifier) @label)
