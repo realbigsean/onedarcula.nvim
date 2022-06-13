849904473440062a2e1f8a4dbea4c6a8c0fa1b2c
realbigsean <seananderson33@gmail.com>
2022-06-12 21:25:20 -0400

    test

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -3,8 +3,8 @@
 (scoped_identifier
   path: (identifier) @path_name (#match? @path_name "^[A-Z]") 
   name: (identifier) @constant)
-(identifier) @keyword
-(#match? @keyword "Self")
-(identifier) @keyword
-(#match? @keyword "crate")
+((identifier) @banana 
+(#match? @banana "Self") @keyword)
+((identifier) @bananas
+(#match? @bananas "crate") @keyword)
 
