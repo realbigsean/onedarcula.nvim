d115d899171319b95ab7b30a03788508ce63a000
realbigsean <seananderson33@gmail.com>
2022-06-12 21:13:00 -0400

    test

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -1,7 +1,7 @@
 (constrained_type_parameter 
   left: (type_identifier) @label)
 (scoped_identifier
-  path: (identifier) @path_name (#match? @path_name "^[A-Z]+[a-z_]+") 
+  path: (identifier) @path_name (#match? @path_name "^[A-Z]") 
   name: (identifier) @constant)
-["Self" "crate"] @keyword
+["Self" "crate" (identifier)] @keyword
 
