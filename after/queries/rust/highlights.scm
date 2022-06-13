4469f11790d06566af24b0de33db0fb9376db9e5
realbigsean <seananderson33@gmail.com>
2022-06-12 22:08:16 -0400

    test

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -1,8 +1,9 @@
 (constrained_type_parameter 
   left: (type_identifier) @label)
-(namespace
-  path: (identifier) @path_name (#match? @path_name "^[A-Z]") 
-  name: (identifier) @constant)
+
 ((type_identifier) @banana 
 (#match? @banana "Self")) @keyword
+
 (crate) @keyword
+
+
