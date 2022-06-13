f8465d7c0866ef43ebe33ac19396fc5d65f99f3c
realbigsean <seananderson33@gmail.com>
2022-06-13 07:16:53 -0400

    test

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -13,3 +13,6 @@
   name: ((identifier) @toast (#lua-match? @toast "^[A-Z]")) @constant)
 
 (shorthand_field_initializer (identifier) @variable) 
+
+(line_comment) @comment (#lua-match? @comment "^[\/][\/][\/|!]") @text.note
+
