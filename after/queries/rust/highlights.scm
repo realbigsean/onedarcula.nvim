46b823266104b338778e66d081bbe5bb8bbd0d10
realbigsean <seananderson33@gmail.com>
2022-06-13 08:53:15 -0400

    test

--- after/queries/rust/highlights.scm
+++ after/queries/rust/highlights.scm
@@ -1,12 +1,12 @@
 (constrained_type_parameter 
-  left: (type_identifier) @type.builtin)
+  left: (type_identifier) @label)
 
 ((type_identifier) @banana 
 (#match? @banana "Self")) @keyword
 
 (crate) @keyword
 
-((type_identifier) @butter (#lua-match? @butter "^[A-Z]$")) @type.builtin
+((type_identifier) @butter (#lua-match? @butter "^[A-Z]$")) @label
 
 (scoped_identifier
   path: ((identifier) @butter (#lua-match? @butter "^[A-Z]")) 
